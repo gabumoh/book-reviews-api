2efd876d90bae27280f49ee6ff2de6ddf35c8db8
gabumoh <gmaumoh@gmail.com>
2018-09-16 14:53:04 +0100

    adding unauthenticated error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -52,5 +53,10 @@ class Handler extends ExceptionHandler
             ], 404);
         }
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticated.'], 401);
     }
 }
